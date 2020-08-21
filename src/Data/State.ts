37f8acfc3c0a1a4182b24b78154a9032ebf9ad64
Thant Sin Toe <mr.thantsintoe@gmail.com>
2020-08-21 20:01:49 +0630

    process STATE data from data sender. Expose API for /statehashes

--- src/Data/State.ts
+++ src/Data/State.ts
@@ -1,3 +1,5 @@
+import { Cycle, currentCycleCounter } from './Cycles'
+import * as Storage from '../Storage'
 export type PartitionHashes = Map<
   number,
   string
@@ -6,7 +8,19 @@ export type PartitionHashes = Map<
 export type NetworkStateHash = string
 
 export interface StateHashes {
-  counter: number
-  partitionHashes: PartitionHashes
+  counter: Cycle['counter']
+  partitionHashes: object
   networkHash: NetworkStateHash
+}
+
+export function processStateHashes(stateHashes: StateHashes[]) {
+  for (const stateHash of stateHashes) {
+    // Skip if already processed [TODO] make this check more secure
+    if (stateHash.counter < currentCycleCounter) continue
+
+    // Save the cycle to db
+    Storage.storeStateHashes(stateHash)
+
+    console.log(`Processed state ${stateHash.counter}`)
+  }
 }
