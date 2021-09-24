ca0edfca0945039ff8be37e720414958ce078286
Aamir Syed <asyed94@gmail.com>
2021-09-24 11:37:35 -0500

    Fixes some TS compile errors regarding process.resourceUsage

--- src/profiler/memoryReporting.ts
+++ src/profiler/memoryReporting.ts
@@ -1,161 +1,160 @@
 const NS_PER_SEC = 1e9
 
-const os = require('os');
-import * as fastify from "fastify";
+const os = require('os')
+import * as fastify from 'fastify'
 
-const process = require('process');
-import { resourceUsage } from 'process';
+const process = require('process')
 
 // process.hrtime.bigint()
 
-interface MemoryReporting { }
+interface MemoryReporting {}
 
 type CounterMap = Map<string, CounterNode>
 interface CounterNode {
-    count: number
-    subCounters: CounterMap
+  count: number
+  subCounters: CounterMap
 }
 
 export let memoryReportingInstance: MemoryReporting
 
 type MemItem = {
-    category: string
-    subcat: string
-    itemKey: string
-    count: number
+  category: string
+  subcat: string
+  itemKey: string
+  count: number
 }
 
 class MemoryReporting {
-    report: MemItem[]
-    lastCPUTimes: any[]
-    server: fastify.FastifyInstance
+  report: MemItem[]
+  lastCPUTimes: any[]
+  server: fastify.FastifyInstance
 
-    constructor(server: fastify.FastifyInstance) {
-        memoryReportingInstance = this
-        this.report = []
-        this.server = server
-        this.lastCPUTimes = this.getCPUTimes()
+  constructor(server: fastify.FastifyInstance) {
+    memoryReportingInstance = this
+    this.report = []
+    this.server = server
+    this.lastCPUTimes = this.getCPUTimes()
+  }
+
+  registerEndpoints() {
+    this.server.get('/memory', (req, res) => {
+      let toMB = 1 / 1000000
+      let report = process.memoryUsage()
+      let outputStr = ''
+      outputStr += `System Memory Report.  Timestamp: ${Date.now()}\n`
+      outputStr += `rss: ${(report.rss * toMB).toFixed(2)} MB\n`
+      outputStr += `heapTotal: ${(report.heapTotal * toMB).toFixed(2)} MB\n`
+      outputStr += `heapUsed: ${(report.heapUsed * toMB).toFixed(2)} MB\n`
+      outputStr += `external: ${(report.external * toMB).toFixed(2)} MB\n`
+      outputStr += `arrayBuffers: ${(report.arrayBuffers * toMB).toFixed(
+        2
+      )} MB\n\n\n`
+
+      this.gatherReport()
+      outputStr = this.reportToStream(this.report, outputStr, 0)
+      res.send(outputStr)
+    })
+
+    // this.server.get('memory-gc', (req, res) => {
+    //     res.write(`System Memory Report.  Timestamp: ${Date.now()}\n`)
+    //     try {
+    //         if (global.gc) {
+    //             global.gc();
+    //             res.write('garbage collected!');
+    //         } else {
+    //             res.write('No access to global.gc.  run with node --expose-gc');
+    //         }
+    //     } catch (e) {
+    //         res.write('ex:No access to global.gc.  run with node --expose-gc');
+    //     }
+    //     res.end()
+    // })
+  }
+
+  addToReport(
+    category: string,
+    subcat: string,
+    itemKey: string,
+    count: number
+  ) {
+    let obj = { category, subcat, itemKey, count }
+    this.report.push(obj)
+  }
+
+  reportToStream(report: MemItem[], outputStr: string, indent: any) {
+    let indentText = '___'.repeat(indent)
+    for (let item of report) {
+      let { category, subcat, itemKey, count } = item
+      let countStr = `${count}`
+      outputStr += `${countStr.padStart(10)} ${category} ${subcat} ${itemKey}\n`
+    }
+    return outputStr
+  }
+
+  gatherReport() {
+    this.report = []
+    this.systemProcessReport()
+  }
+
+  getCPUTimes() {
+    const cpus = os.cpus()
+    let times = []
+
+    for (let cpu of cpus) {
+      let timeObj: any = {}
+      let total = 0
+      for (const [key, value] of Object.entries(cpu.times)) {
+        let time = Number(value)
+        total += time
+        timeObj[key] = value
+      }
+      timeObj['total'] = total
+
+      times.push(timeObj)
+    }
+    return times
+  }
+
+  cpuPercent() {
+    let currentTimes = this.getCPUTimes()
+
+    let deltaTimes: any = []
+    let percentTimes: any = []
+
+    let percentTotal = 0
+
+    for (let i = 0; i < currentTimes.length; i++) {
+      const currentTimeEntry = currentTimes[i]
+      const lastTimeEntry = this.lastCPUTimes[i]
+      let deltaTimeObj: any = {}
+      for (const [key, value] of Object.entries(currentTimeEntry)) {
+        deltaTimeObj[key] = currentTimeEntry[key] - lastTimeEntry[key]
+      }
+      deltaTimes.push(deltaTimeObj)
+
+      for (const [key, value] of Object.entries(currentTimeEntry)) {
+        percentTimes[key] = deltaTimeObj[key] / deltaTimeObj['total']
+      }
+
+      percentTotal += percentTimes['user'] || 0
+      percentTotal += percentTimes['nice'] || 0
+      percentTotal += percentTimes['sys'] || 0
     }
 
-    registerEndpoints() {
-        this.server.get('/memory', (req, res) => {
-            let toMB = 1 / 1000000
-            let report = process.memoryUsage()
-            let outputStr = ''
-            outputStr += `System Memory Report.  Timestamp: ${Date.now()}\n`
-            outputStr += `rss: ${(report.rss * toMB).toFixed(2)} MB\n`
-            outputStr += `heapTotal: ${(report.heapTotal * toMB).toFixed(2)} MB\n`
-            outputStr += `heapUsed: ${(report.heapUsed * toMB).toFixed(2)} MB\n`
-            outputStr += `external: ${(report.external * toMB).toFixed(2)} MB\n`
-            outputStr += `arrayBuffers: ${(report.arrayBuffers * toMB).toFixed(2)} MB\n\n\n`
+    this.lastCPUTimes = currentTimes
+    let percentUsed = percentTotal / currentTimes.length
+    return percentUsed
+  }
 
-            this.gatherReport()
-            outputStr = this.reportToStream(this.report, outputStr, 0)
-            res.send(outputStr)
-        })
-
-        // this.server.get('memory-gc', (req, res) => {
-        //     res.write(`System Memory Report.  Timestamp: ${Date.now()}\n`)
-        //     try {
-        //         if (global.gc) {
-        //             global.gc();
-        //             res.write('garbage collected!');
-        //         } else {
-        //             res.write('No access to global.gc.  run with node --expose-gc');
-        //         }
-        //     } catch (e) {
-        //         res.write('ex:No access to global.gc.  run with node --expose-gc');
-        //     }
-        //     res.end()
-        // })
-
-
+  systemProcessReport() {
+    this.addToReport('Process', 'CPU', 'cpuPercent', this.cpuPercent())
+    // let avgCPU = this.shardus.statistics.getAverage('cpuPercent')
+    // this.addToReport('Process','CPU', 'cpuAVGPercent', avgCPU )
+    let report = process.resourceUsage()
+    for (const [key, value] of Object.entries(report)) {
+      this.addToReport('Process', 'Details', key, value as number)
     }
-
-    addToReport(category: string, subcat: string, itemKey: string, count: number) {
-        let obj = { category, subcat, itemKey, count }
-        this.report.push(obj)
-    }
-
-    reportToStream(report: MemItem[], outputStr: string, indent: any) {
-
-        let indentText = '___'.repeat(indent)
-        for (let item of report) {
-            let { category, subcat, itemKey, count } = item
-            let countStr = `${count}`
-            outputStr += `${countStr.padStart(10)} ${category} ${subcat} ${itemKey}\n`
-        }
-        return outputStr
-    }
-
-    gatherReport() {
-        this.report = []
-        this.systemProcessReport()
-    }
-
-    getCPUTimes() {
-        const cpus = os.cpus();
-        let times = []
-
-        for (let cpu of cpus) {
-            let timeObj: any = {}
-            let total = 0
-            for (const [key, value] of Object.entries(cpu.times)) {
-                let time = Number(value)
-                total += time
-                timeObj[key] = value
-            }
-            timeObj['total'] = total
-
-            times.push(timeObj)
-        }
-        return times
-    }
-
-    cpuPercent() {
-
-        let currentTimes = this.getCPUTimes()
-
-        let deltaTimes: any = []
-        let percentTimes: any = []
-
-        let percentTotal = 0
-
-        for (let i = 0; i < currentTimes.length; i++) {
-            const currentTimeEntry = currentTimes[i];
-            const lastTimeEntry = this.lastCPUTimes[i]
-            let deltaTimeObj: any = {}
-            for (const [key, value] of Object.entries(currentTimeEntry)) {
-                deltaTimeObj[key] = currentTimeEntry[key] - lastTimeEntry[key]
-            }
-            deltaTimes.push(deltaTimeObj)
-
-            for (const [key, value] of Object.entries(currentTimeEntry)) {
-                percentTimes[key] = deltaTimeObj[key] / deltaTimeObj['total']
-            }
-
-            percentTotal += (percentTimes['user'] || 0)
-            percentTotal += (percentTimes['nice'] || 0)
-            percentTotal += (percentTimes['sys'] || 0)
-
-        }
-
-        this.lastCPUTimes = currentTimes
-        let percentUsed = percentTotal / currentTimes.length
-        return percentUsed
-    }
-
-    systemProcessReport() {
-        this.addToReport('Process', 'CPU', 'cpuPercent', this.cpuPercent())
-        // let avgCPU = this.shardus.statistics.getAverage('cpuPercent')
-        // this.addToReport('Process','CPU', 'cpuAVGPercent', avgCPU )
-        let report = resourceUsage()
-        for (const [key, value] of Object.entries(report)) {
-            this.addToReport('Process', 'Details', key, value)
-        }
-
-    }
-
+  }
 }
 
 export default MemoryReporting
