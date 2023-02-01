575534ed802061b6330d5f88848d38a0b6320b4a
jairajdev <jairajranamagar.dev@gmail.com>
2023-02-01 19:02:39 +0900

    Change data sender timeout back to 5 min

--- src/Config.ts
+++ src/Config.ts
@@ -39,7 +39,7 @@ let config: Config = {
   ARCHIVER_EXISTING: [],
   ARCHIVER_LOGS: 'archiver-logs',
   ARCHIVER_DB: 'archiver-db',
-  DATASENDER_TIMEOUT: 1000 * 60 * 2,
+  DATASENDER_TIMEOUT: 1000 * 60 * 5,
   RATE_LIMIT: 100, // 100 req per second,
   N_NODELIST: 30, // number of active node list GET /nodelist should emit but if the total active nodelist is less than said value it will emit all the node list.
   STATISTICS: {
