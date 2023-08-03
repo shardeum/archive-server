34326604026c672a919213945310ebfae0f8c955
municorn <municorn@musicaloft.com>
2023-08-03 15:04:58 -0500

    Add sync-v2

---
+++ src/sync-v2/index.ts
@@ -0,0 +1,107 @@
+/**
+ * SyncV2 a p2p module that contains all of the functionality for the new
+ * Node List Sync v2.
+ */
+
+import { ResultAsync } from 'neverthrow'
+import { hexstring, P2P } from '@shardus/types'
+import {
+  getCurrentCycleDataFromNode,
+  robustQueryForCycleRecordHash,
+  robustQueryForValidatorListHash,
+  getValidatorListFromNode,
+} from './queries'
+import { ArchiverNodeInfo } from '../State'
+import { getActiveNodeListFromArchiver } from '../NodeList'
+import * as NodeList from '../NodeList'
+
+/**
+ * Given a list of archivers, queries each one until one returns an active node list.
+ *
+ * The endpoint queried does not return a *full* list of nodes. It's a partial
+ * list that will be enough to use in robust queries.
+ */
+async function getActiveListFromSomeArchiver(
+  archivers: ArchiverNodeInfo[]
+): Promise<P2P.SyncTypes.ActiveNode[]> {
+  for (const archiver of archivers) {
+    try {
+      const nodeList = await getActiveNodeListFromArchiver(archiver)
+      if (nodeList) {
+        return nodeList
+      }
+    } catch (e) {
+      console.warn(`failed to get active node list from archiver ${archiver.ip}:${archiver.port}: ${e}`)
+      continue
+    }
+  }
+
+  // all archivers have failed at this point
+  throw new Error('no archiver could return an active node list')
+}
+
+/**
+  * Synchronizes the NodeList and gets the latest CycleRecord from other validators.
+  */
+export function syncV2(activeArchivers: ArchiverNodeInfo[]): ResultAsync<P2P.CycleCreatorTypes.CycleRecord, Error> {
+  return ResultAsync.fromPromise(getActiveListFromSomeArchiver(activeArchivers), (e: Error) => e).andThen(
+    (nodeList) =>
+      syncValidatorList(nodeList).andThen((validatorList) =>
+        syncLatestCycleRecordAndMarker(nodeList).map(([cycle, cycleMarker]) => {
+          // validatorList needs to be transformed into a ConsensusNodeInfo[]
+          const consensusNodeList: NodeList.ConsensusNodeInfo[] = validatorList.map((node) => ({
+            publicKey: node.publicKey,
+            ip: node.externalIp,
+            port: node.externalPort,
+            id: node.id,
+          }))
+
+          NodeList.addNodes(NodeList.Statuses.ACTIVE, cycleMarker, consensusNodeList)
+
+          return cycle
+        })
+      )
+  )
+}
+
+/**
+ * This function synchronizes a validator list from `activeNodes`.
+ *
+ * @param {P2P.SyncTypes.ActiveNode[]} activeNodes - An array of active nodes to be queried.
+ * The function first performs a robust query for the latest node list hash.
+ * After obtaining the hash, it retrieves the full node list from one of the winning nodes.
+ *
+ * @returns {ResultAsync<P2P.NodeListTypes.Node[], Error>} - A ResultAsync object. On success, it will contain
+ * an array of Node objects, and on error, it will contain an Error object. The function is asynchronous
+ * and can be awaited.
+ */
+function syncValidatorList(
+  activeNodes: P2P.SyncTypes.ActiveNode[]
+): ResultAsync<P2P.NodeListTypes.Node[], Error> {
+  // run a robust query for the lastest node list hash
+  return robustQueryForValidatorListHash(activeNodes).andThen(({ value, winningNodes }) =>
+    // get full node list from one of the winning nodes
+    getValidatorListFromNode(winningNodes[0], value.nodeListHash))
+}
+
+/**
+ * Synchronizes the latest cycle record from a list of active nodes.
+ *
+ * @param {P2P.SyncTypes.ActiveNode[]} activeNodes - An array of active nodes to be queried.
+ * The function first performs a robust query for the latest cycle record hash.
+ * After obtaining the hash, it retrieves the current cycle data from one of the winning nodes.
+ *
+ * @returns {ResultAsync<P2P.CycleCreatorTypes.CycleRecord, Error>} - A ResultAsync object.
+ * On success, it will contain a CycleRecord object, and on error, it will contain an Error object.
+ * The function is asynchronous and can be awaited.
+ */
+function syncLatestCycleRecordAndMarker(
+  activeNodes: P2P.SyncTypes.ActiveNode[]
+): ResultAsync<[P2P.CycleCreatorTypes.CycleRecord, hexstring], Error> {
+  // run a robust query for the latest cycle record hash
+  return robustQueryForCycleRecordHash(activeNodes).andThen(({ value: cycleRecordHash, winningNodes }) =>
+    // get current cycle record from node
+    getCurrentCycleDataFromNode(winningNodes[0], cycleRecordHash).map((cycle) =>
+      [cycle, cycleRecordHash] as [P2P.CycleCreatorTypes.CycleRecord, hexstring])
+  )
+}
---
+++ src/sync-v2/queries.ts
@@ -0,0 +1,152 @@
+/**
+ * `queries` submodule. Contains logic pertaining to anything that
+ * requires an external node to be queried, including robust queries.
+ *
+ * Code is duplicated from shardus-global-server.
+ */
+
+import { hexstring, P2P } from '@shardus/types'
+import { errAsync, ResultAsync } from 'neverthrow'
+import { attempt, robustQuery } from '../Utils'
+import { get } from '../P2P'
+
+/** A successful RobustQueryResult whose value is an unwrapped `Ok` `Result`. */
+type UnwrappedRobustResult<N, V> = {
+  winningNodes: N[]
+  value: V
+}
+
+// Convenience type aliases.
+type ActiveNode = P2P.SyncTypes.ActiveNode
+type Validator = P2P.NodeListTypes.Node
+type Archiver = P2P.ArchiversTypes.JoinedArchiver
+type CycleRecord = P2P.CycleCreatorTypes.CycleRecord
+
+/** A `ResultAsync` that wraps an `UnwrappedRobustResult`. */
+export type RobustQueryResultAsync<T> = ResultAsync<UnwrappedRobustResult<ActiveNode, T>, Error>
+
+const MAX_RETRIES = 3
+const REDUNDANCY = 3
+
+async function throwableQueryFunction<T>(node: ActiveNode, endpointName: string, params: Record<string, string>): Promise<T> {
+  let url = `http://${node.ip}:${node.port}/${endpointName}`;
+  if (params) {
+    const encodedParams = new URLSearchParams(params).toString();
+    url += `?${encodedParams}`;
+  }
+
+  const res = await get(url)
+
+  if (res.ok) {
+    return (await res.json()) as T
+  } else {
+    throw new Error(`get failed with status ${res.statusText}`)
+  }
+}
+
+/**
+ * Executes a robust query to a specified endpoint across multiple nodes, providing more fault tolerance.
+ *
+ * @param {ActiveNode[]} nodes - An array of active nodes to query.
+ * @param {string} endpointName - The name of the endpoint to call on the active nodes.
+ *
+ * The function runs a robust query, logs the query, and ensures the result of the query is robust.
+ * It makes an HTTP GET request to the specified endpoint for each node, and if the call fails,
+ * an error message is returned.
+ *
+ * @returns {RobustQueryResultAsync<T>} - A ResultAsync object. On success, it contains a result object with
+ * the winning nodes and the returned value. On failure, it contains an Error object. The function is asynchronous
+ * and can be awaited.
+ */
+function makeRobustQueryCall<T>(nodes: ActiveNode[], endpointName: string): RobustQueryResultAsync<T> {
+  // query function that makes the endpoint call as specified
+  const queryFn = (node: ActiveNode): ResultAsync<T, Error> => ResultAsync.fromPromise(throwableQueryFunction(node, endpointName, {}), (err) => new Error(`couldn't query ${endpointName}: ${err}`))
+
+  // run the robust query, wrapped in an async Result return the unwrapped result (with `map`) if successful
+  const logPrefix = `syncv2-robust-query-${endpointName}`
+  return ResultAsync.fromPromise(
+    attempt(() => robustQuery(nodes, queryFn), {
+      maxRetries: MAX_RETRIES,
+      logPrefix,
+    }),
+    (err) => new Error(`robust query failed for ${endpointName}: ${err}`)
+  ).andThen((robustResult) => {
+    // ensure the result was robust as well.
+    if (!(robustResult.count >= Math.min(REDUNDANCY, nodes.length))) {
+      return errAsync(new Error(`result of ${endpointName} wasn't robust`))
+    }
+    return robustResult.value.map((value) => ({
+      winningNodes: robustResult.nodes,
+      value,
+    }))
+  })
+}
+
+/**
+ * Performs a simple fetch operation from a given node to a specified endpoint. The operation is retried up to
+ * MAX_RETRIES times in case of failure.
+ *
+ * @param {ActiveNode} node - An active node to fetch data from.
+ * @param {string} endpointName - The name of the endpoint to fetch data from.
+ *
+ * The function attempts to make an HTTP GET request to the specified endpoint. If the call fails,
+ * it retries the operation and returns an error message.
+ *
+ * @returns {ResultAsync<T, Error>} - A ResultAsync object. On success, it will contain the data fetched, and
+ * on error, it will contain an Error object. The function is asynchronous and can be awaited.
+ */
+function attemptSimpleFetch<T>(
+  node: ActiveNode,
+  endpointName: string,
+  params: Record<string, string>
+): ResultAsync<T, Error> {
+  return ResultAsync.fromPromise(
+    attempt(async () => throwableQueryFunction(node, endpointName, params), {
+      maxRetries: MAX_RETRIES,
+      logPrefix: `syncv2-simple-fetch-${endpointName}`,
+    }),
+    (err) => new Error(`simple fetch failed for ${endpointName}: ${err}`)
+  )
+}
+
+/** Executes a robust query to retrieve the cycle marker from the network. */
+export function robustQueryForCycleRecordHash(nodes: ActiveNode[]): RobustQueryResultAsync<hexstring> {
+  return makeRobustQueryCall(nodes, 'current-cycle-hash')
+}
+
+/** Executes a robust query to retrieve the validator list hash and next cycle timestamp from the network. */
+export function robustQueryForValidatorListHash(
+  nodes: ActiveNode[]
+): RobustQueryResultAsync<{ nodeListHash: hexstring, nextCycleTimestamp: number }> {
+  return makeRobustQueryCall(nodes, 'validator-list-hash')
+}
+
+/** Executes a robust query to retrieve the archiver list hash from the network. */
+export function robustQueryForArchiverListHash(
+  nodes: ActiveNode[]
+): RobustQueryResultAsync<{ archiverListHash: hexstring }> {
+  return makeRobustQueryCall(nodes, 'archiver-list-hash')
+}
+
+/** Retrives the entire last cycle from the node. */
+export function getCurrentCycleDataFromNode(node: ActiveNode, expectedMarker: hexstring): ResultAsync<CycleRecord, Error> {
+  return attemptSimpleFetch(node, 'cycle-by-marker', {
+    marker: expectedMarker
+  })
+}
+
+/** Gets the full validator list from the specified node. */
+export function getValidatorListFromNode(node: ActiveNode, expectedHash: hexstring): ResultAsync<Validator[], Error> {
+  console.log(`getting validator list from ${node.ip}:${node.port} with hash ${expectedHash}`)
+  return attemptSimpleFetch(node, 'validator-list', {
+    hash: expectedHash
+  })
+}
+
+/** Gets the full node list from the specified archiver. */
+export function getArchiverListFromNode(node: ActiveNode, expectedHash: hexstring): ResultAsync<Archiver[], Error> {
+  console.log(`getting archiver list from ${node.ip}:${node.port} with hash ${expectedHash}`)
+  return attemptSimpleFetch(node, 'archiver-list', {
+    hash: expectedHash
+  })
+}
