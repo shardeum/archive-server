ab253b591f937136763bad1ac76cc7f2bca15de5
municorn <municorn@musicaloft.com>
2023-08-04 13:17:26 -0500

    module-declarations: Remove crypto-utils module declaration
    @shardus/crypto-utils supplies its own type definitions

--- src/module-declarations.d.ts
+++ src/module-declarations.d.ts
@@ -29,39 +29,4 @@ declare module 'shardus-crypto-types' {
   }
 }
 
-declare module '@shardus/crypto-utils' {
-  import * as CryptoTypes from 'shardus-crypto-types'
-
-  interface ShardusCrypto {
-    init: (hashKey: CryptoTypes.hexstring) => void
-    hashObj: (
-      obj: CryptoTypes.LooseObject,
-      removeSign?: boolean,
-      removeTag?: boolean
-    ) => CryptoTypes.hexstring
-    generateKeypair: () => CryptoTypes.Keypair
-    stringify: (obj: CryptoTypes.LooseObject) => string
-    signObj: (
-      obj: CryptoTypes.LooseObject,
-      secretKey: CryptoTypes.secretKey,
-      publicKey: CryptoTypes.publicKey
-    ) => void
-    verifyObj: (obj: CryptoTypes.SignedObject) => boolean
-    tagObj: (obj: CryptoTypes.LooseObject, sharedK: CryptoTypes.sharedKey) => void
-    authenticateObj: (obj: CryptoTypes.TaggedObject, sharedK: CryptoTypes.sharedKey) => boolean
-    convertSkToCurve: (sk: CryptoTypes.secretKey) => CryptoTypes.curveSecretKey
-    convertPkToCurve: (pk: CryptoTypes.publicKey) => CryptoTypes.curvePublicKey
-    generateSharedKey: (
-      curveSk: CryptoTypes.curveSecretKey,
-      curvePk: CryptoTypes.curvePublicKey
-    ) => CryptoTypes.sharedKey
-
-    hash(input: string): CryptoTypes.hexstring
-  }
-
-  const crypto: ShardusCrypto
-
-  export = crypto
-}
-
 declare module 'minimist'
