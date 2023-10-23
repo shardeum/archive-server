57ff9ebb9aefc793c891c9e1370aeeab6bfacc05
Charles Esterbrook <chuck@shardeum.org>
2023-10-23 10:21:58 -0700

    feat(LostArchivers): Add a starting message to stdout and stderr. This is useful in general, but also helps confirm that we're running the custom archiver for lost archiver detection.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
+const _startingMessage = `shardus-archive server starting at
+locale:  {new Date().toLocaleString()}
+ISO/UTC: {new Date().toISOString()}`
+console.log(_startingMessage)
+console.error(_startingMessage)
+
 import { join } from 'path'
 import fastify, { FastifyInstance, FastifyRequest } from 'fastify'
 import fastifyCors from '@fastify/cors'
@@ -1589,14 +1595,14 @@ async function startServer() {
       if (enableLoseYourself) {
         Logger.mainLogger.debug('/lose-yourself: exit(1)')
 
-        reply.send(Crypto.sign({'status': 'success', 'message': 'will exit'}))
+        reply.send(Crypto.sign({ status: 'success', message: 'will exit' }))
 
         // We don't call exitArchiver() here because that awaits Data.sendLeaveRequest(...),
         // but we're simulating a lost node.
         process.exit(1)
       } else {
         Logger.mainLogger.debug('/lose-yourself: not enabled. no action taken.')
-        reply.send(Crypto.sign({'status': 'failure', 'message': 'not enabled'}))
+        reply.send(Crypto.sign({ status: 'failure', message: 'not enabled' }))
         // set enableLoseYourself to true--but never commit!
       }
     }
