957856d71b12ad19a2e0aa9d8321d157f78a227f
Caralee Jackson <tiedyedsheepengineering@gmail.com>
2024-07-22 12:19:25 -0500

    Revert "prettier fixes"
    This reverts commit 776a128a3af48f0537ba4138c536294aa14d9a9d.

--- src/sync-v2/queries.ts
+++ src/sync-v2/queries.ts
@@ -119,9 +119,7 @@ function attemptSimpleFetch<T>(
 }
 
 /** Executes a robust query to retrieve the cycle marker from the network. */
-export function robustQueryForCycleRecordHash(
-  nodes: ActiveNode[]
-): RobustQueryResultAsync<{ currentCycleHash: hexstring }> {
+export function robustQueryForCycleRecordHash(nodes: ActiveNode[]): RobustQueryResultAsync<{ currentCycleHash: hexstring }> {
   return makeRobustQueryCall(nodes, 'current-cycle-hash')
 }
 
