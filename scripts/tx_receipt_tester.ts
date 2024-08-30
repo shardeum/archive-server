b64c3b46947d97cb4d521a74f758fdd2b77841af
Tanuj Soni <tanuj@shardeum.org>
2024-08-30 16:25:28 +0530

    Add script to get data from /totalData and /receipts endpoint

---
+++ scripts/tx_receipt_tester.ts
@@ -0,0 +1,39 @@
+import * as crypto from '@shardus/crypto-utils'
+import fetch from 'node-fetch'
+import { join } from 'path'
+import { config, overrideDefaultConfig } from '../src/Config'
+import { Utils as StringUtils } from '@shardus/types'
+const configFile = join(process.cwd(), 'archiver-config.json')
+overrideDefaultConfig(configFile)
+crypto.init(config.ARCHIVER_HASH_KEY)
+const devAccount = {
+  publicKey: config.ARCHIVER_PUBLIC_KEY,
+  secretKey: config.ARCHIVER_SECRET_KEY,
+}
+const ARCHIVER_URL = `http://127.0.0.1:4000`
+const data: any = {
+  startCycle: 0,
+  endCycle: 50,
+  type: 'tally',
+  sender: devAccount.publicKey,
+}
+crypto.signObj(data, devAccount.secretKey, devAccount.publicKey)
+// console.log(data)
+// Update endpoints name ... totalData / cycleinfo / receipt / account / transaction
+// fetch(`${ARCHIVER_URL}/totalData`, {
+// fetch(`${ARCHIVER_URL}/cycleinfo`, {
+fetch(`${ARCHIVER_URL}/receipt`, {
+  // fetch(`${ARCHIVER_URL}/account`, {
+  method: 'post',
+  body: StringUtils.safeStringify(data),
+  headers: { 'Content-Type': 'application/json' },
+  timeout: 2000,
+})
+  .then(async (res) => {
+    if (res.ok) console.log(await res.json())
+    // if (res.ok) console.dir(await res.json(), { depth: null })
+    else console.log(res.status)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
